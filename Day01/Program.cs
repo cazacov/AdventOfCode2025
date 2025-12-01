da8aa53f09466c3223f22e16f64dfa2d8137ce08
Victor Kazakov <cazacov.git@gmail.com>
2025-12-02 00:22:53 +0100

    Day 1, puzzle 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Day01
+﻿namespace Day01
 {
     internal class Program
     {
@@ -9,7 +7,7 @@ namespace Day01
             Console.WriteLine("Advent of Code 2025, Day 1");
             var input = ReadInput("input.txt");
             Puzzle1(input);
-            //Puzzle2(input);
+            Puzzle2(input);
         }
 
         private static void Puzzle1(int[] input)
@@ -29,13 +27,38 @@ namespace Day01
             Console.WriteLine(count);
         }
 
+        private static void Puzzle2(int[] input)
+        {
+            Console.Write("Puzzle 2: ");
+            var pos = 50;
+            var count = 0;
+            foreach (var distance in input)
+            {
+                var newPos = pos + distance;
+                if (newPos > 0)
+                {
+                    count += newPos / 100;
+                }
+                else
+                {
+                    count += pos > 0 ? 1 : 0;   //starting point was positive, destination is negative -> zero passed at least once
+                    count += -newPos / 100;     // repeated zeros
+                }
+                pos = newPos % 100;
+                if (pos < 0)
+                {
+                    pos += 100;
+                }
+            }
+            Console.WriteLine(count);
+        }
 
         private static int[] ReadInput(string fileName)
         {
             var lines = File.ReadAllLines(fileName);
             return lines.Select(x =>
             {
-                var n = int.Parse(x.Substring(1));
+                var n = int.Parse(x[1..]);
                 return x[0] == 'R' ? n : -n;
             }).ToArray();
         }
