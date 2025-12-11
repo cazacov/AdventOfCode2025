c4712939a30e69831ef82c989b99d26e073dd314
Victor Kazakov <cazacov.git@gmail.com>
2025-12-11 23:46:17 +0100

    Day 11 puzzle 1

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,17 +1,112 @@
-﻿namespace Day11
+﻿using static Day11.Program;
+
+namespace Day11
 {
     //  Advent of Code 2025, day 11: https://adventofcode.com/2025/day/11
     internal class Program
     {
+        public class Device
+        {
+            public string Name;
+            public List<String> Outs = new List<String>();
+            public long PathCount = 0;
+            public int Distance = Int32.MaxValue;
+
+            public override bool Equals(object? obj)
+            {
+                return this.Equals(obj as Device);
+            }
+
+            public bool Equals(Device other)
+            {
+                if (other == null) return false;
+                return this.Name == other.Name;
+            }
+
+            public override int GetHashCode()
+            {
+                return Name.GetHashCode();
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2025, Day 11");
-            var input = ReadInput("input.txt");
+            var devices = ReadInput("input.txt");
+            if (!devices.ContainsKey("out"))
+            {
+                devices["out"] = new Device() { Name = "out" };
+            }
+            Puzzle1(devices);
+            Puzzle2(devices);
         }
 
-        private static string[] ReadInput(string fileName)
+        private static void Puzzle1(Dictionary<string, Device> devices)
         {
-            return File.ReadAllLines(fileName);
+            Console.WriteLine(PathCount("you", "out", devices));
+
+        }
+
+        private static void Puzzle2(Dictionary<string, Device> devices)
+        {
+            // svr -> fft ->  dac -> out
+            var n1 = PathCount("svr", "fft", devices) * PathCount("fft", "dac", devices) * PathCount("dac", "out", devices);
+
+            // svr -> dac ->  fft -> out
+            var n2 = PathCount("svr", "dac", devices) * PathCount("dac", "fft", devices) * PathCount("fft", "out", devices);
+            Console.WriteLine(n1 + n2);
+        }
+
+        private static long PathCount(string start, string target, Dictionary<string, Device> devices)
+        {
+            if (!devices.ContainsKey(target))
+            {
+                devices[target] = new Device() { Name = target};
+            }
+            foreach (var device in devices.Values)
+            {
+                device.Distance = Int32.MaxValue;
+                device.PathCount = 0;
+            }
+            devices[start].PathCount = 1;
+            var todo = new HashSet<Device>() { devices[start] };
+            while (todo.Any())
+            {
+                var item = todo.OrderBy(x => x.Distance).First();
+                todo.Remove(item);
+                foreach (var outKey in item.Outs)
+                {
+                    var outItem = devices[outKey];
+                    var d = item.Distance + 1;
+                    if (outItem.Distance > d)
+                    {
+                        outItem.Distance = d;
+                    }
+                    outItem.PathCount += item.PathCount;
+                    todo.Add(outItem);
+                }
+            }
+
+            var result = devices[target].PathCount;
+            return result;
+        }
+
+        private static Dictionary<string, Device> ReadInput(string fileName)
+        {
+            var lines = File.ReadAllLines(fileName);
+            var devices = new List<Device>();
+            foreach (var line in lines)
+            {
+                var i = line.IndexOf(':');
+                var device = new Device()
+                {
+                    Name = line.Substring(0, i)
+                };
+                var outs = line.Substring(i+2).Split(' ');
+                device.Outs.AddRange(outs);
+                devices.Add(device);
+            }
+            return devices.ToDictionary(device => device.Name);
         }
     }
 }
